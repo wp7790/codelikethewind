6b65ab14202a7a96c58d8af495d03e7b0db4fc92
TiOluwa Olarewaju <tolarewa@redhat.com>
2017-07-11 10:42:17 -0500

    initial commit

---
+++ simple-process/src/main/java/org/codelikethewind/process/App.java
@@ -0,0 +1,13 @@
+package org.codelikethewind.process;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
